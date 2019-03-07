360911091d339e69a12238b66836d1d6e1076d45
avinashn686 <48235777+avinashn686@users.noreply.github.com>
2019-03-07 15:43:30 +0530

    Add files via upload

--- medscrap/medscrap/spiders/meds_spider.py
+++ medscrap/medscrap/spiders/meds_spider.py
@@ -1,16 +1,41 @@
 import scrapy
-# from ..items import MedscrapItem
-
-class MedSpider(scrapy.Spider):
+class MedScrap(scrapy.Spider):
     name = 'med'
-    start_urls = [
-        'https://healthapps.dhss.mo.gov/showmeltc/default.aspx',
-    ]
+    start_urls = ['https://healthapps.dhss.mo.gov/showmeltc/default.aspx']
+    # download_delay = 1.5
+
     def parse(self, response):
-    	# items=MedscrapItem()
-    	# drop_box=response.css('select#ContentPlaceHolder1_ddlCounty > option :: attr(value)').extract()
-    	# yield drop_box
-    	for drop in response.css('select#ContentPlaceHolder1_ddlCounty > option :: attr(value)').extract():
-    		yield scrapy.FormRequest('https://healthapps.dhss.mo.gov/showmeltc/default.aspx',formdata = {'drop':response.css('select#ContentPlaceHolder1_ddlCounty > option[selected] ::attr(value)').extract()})
-    # def parse_result(self, response):
-    # 	yield{}
+        dropdown=response.css('select#ContentPlaceHolder1_ddlCounty > option ::attr(value)').extract()
+        for drop in dropdown:
+            yield scrapy.FormRequest(
+                'https://healthapps.dhss.mo.gov/showmeltc/default.aspx',
+                formdata={
+                    # 'author': author,
+                    'ContentPlaceHolder1_ddlCounty' : response.css(
+                        'select#ContentPlaceHolder1_ddlCounty > option[selected] ::attr(value)').extract_first(),
+                    '__VIEWSTATE': response.css('input#__VIEWSTATE::attr(value)').extract_first()
+                },
+                # callback=self.parse_tags
+            )
+
+    # def parse_tags(self, response):
+    #     for tag in response.css('select#tag > option ::attr(value)').extract():
+    #         yield scrapy.FormRequest(
+    #             'http://quotes.toscrape.com/filter.aspx',
+    #             formdata={
+    #                 'author': response.css(
+    #                     'select#author > option[selected] ::attr(value)'
+    #                 ).extract_first(),
+    #                 'tag': tag,
+    #                 '__VIEWSTATE': response.css('input#__VIEWSTATE::attr(value)').extract_first()
+    #             },
+    #             callback=self.parse_results,
+    #         )
+
+    # def parse_results(self, response):
+    #     for quote in response.css("div.quote"):
+    #         yield {
+    #             'quote': quote.css('span.content ::text').extract_first(),
+    #             'author': quote.css('span.author ::text').extract_first(),
+    #             'tag': quote.css('span.tag ::text').extract_first(),
+    #         }
